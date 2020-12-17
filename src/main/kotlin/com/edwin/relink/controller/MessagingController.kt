d6bd2465aee68e9d9451f63b36e30cca2452a7dc
E-D-W-I-N <scvaersoft503@gmail.com>
2020-12-17 23:37:52 +0700

    rabbitMQ integration

---
+++ src/main/kotlin/com/edwin/relink/controller/MessagingController.kt
@@ -0,0 +1,19 @@
+package com.edwin.relink.controller
+
+import com.edwin.relink.MessagingConfig
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MessagingController {
+    @Autowired
+    private val template: RabbitTemplate? = null
+
+    @PostMapping("/send")
+    fun bookOrder() {
+        print("I'm sender and I'm triggered!!!")
+        template!!.convertAndSend(MessagingConfig.EXCHANGE, MessagingConfig.ROUTING_KEY, "pepega")
+    }
+}
